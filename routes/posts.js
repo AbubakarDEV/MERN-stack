c2d904d407b03d297dcba14bb3df6ef5c4ebfc2e
abubakarDev <abub1156@gmail.com>
2021-10-17 19:16:05 +0500

    initial commit

---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+// import {
+//   getPosts,
+//   //   getPost,
+//   //   createPost,
+//   //   updatePost,
+//   //   likePost,
+//   //   deletePost,
+// } from "../controlers/post";
+
+const router = express.Router();
+// router.get("/", getPosts);
+// router.post("/", createPost);
+// router.get("/:id", getPost);
+// router.patch("/:id", updatePost);
+// router.delete("/:id", deletePost);
+// router.patch("/:id/likePost", likePost);
+
+export default router;
