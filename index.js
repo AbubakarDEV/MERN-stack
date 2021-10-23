6253a6fcb8e9f49c3ba62b32b25fe3cd7db6461b
abubakarDev <abub1156@gmail.com>
2021-10-23 12:48:54 +0500

    add routes and controoler

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
+import postRoutes from "./routers/post.js";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from "./routes/posts";
+
 const app = express();
-app.use("/posts", postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://abubakar:abubakar123@cluster0.6m1x8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
